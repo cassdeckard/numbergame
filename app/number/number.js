9f151e476ba94ee9c0c13eb4e3ef753c2351cbb1
Matt Deckard <matthew.d.deckard@gmail.com>
2016-03-12 17:06:43 -0600

    Display three wrong answers

--- app/number/number.js
+++ app/number/number.js
@@ -11,8 +11,11 @@ angular.module('myApp.number', ['ngRoute'])
 }])
 
 .controller('NumberCtrl', ['$scope', '$window', '$timeout', function($scope, $window, $timeout) {
+  $scope.numArray = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
   this.reroll = function() {
-    $scope.targetNumber = randomInt();
+    shuffle($scope.numArray);
+      $scope.targetNumber = $scope.numArray[0];
   }
 
   this.triggerWin = function() {
@@ -26,5 +29,23 @@ angular.module('myApp.number', ['ngRoute'])
 
   function randomInt() {
     return Math.floor((Math.random()*6)+1);
+  }
+
+  function shuffle(array) {
+    var m = array.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+
+    return array;
   }
 }]);
